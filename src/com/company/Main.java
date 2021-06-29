3ed83a3580a8cee64c14a84b1529f575d2c914cc
AlijaJeniceka <alija.juldaseva@gmail.com>
2021-06-29 15:49:38 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String twelve = "12";
+	    String nine = "9";
+	    double twelve12 = Double.parseDouble(twelve);
+        double nine9 = Double.parseDouble(nine);
+        double sum = twelve12 + nine9;
+
+        System.out.println(twelve + " + " + nine + " = " + sum);
+        // 2. Write a program that  adds the value two Strings
+        // //using the wrapper classes to parse the strings.
+        // °5" + "8" = 13
+    }
+}
